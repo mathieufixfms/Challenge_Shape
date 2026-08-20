6cf262f545809003c98417ae2bfeee915608b678
Philippe P <philippe.peyrous@fms-ea.com>
2026-08-20 11:06:34 +0200

    modif Shape ajoutabstract void draw

--- src/fr/entities/Shape.java
+++ src/fr/entities/Shape.java
@@ -1,3 +1,11 @@
-public class Shape {
-    
+import java.awt.Graphics;
+
+
+// Shape représente une forme générale.
+// Point, Circle et Square pourront hériter de cette classe.
+public abstract class Shape {
+
+    // Chaque forme DOIT savoir se dessiner.
+    // Mais la façon de se dessiner dépend de la forme.
+    public abstract void draw(Graphics g);
 }
