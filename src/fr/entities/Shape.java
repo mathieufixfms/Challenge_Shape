16c8f871c2ea3d92e6329c7bec0db360ad0e823d
Raphael Audouard <raphael.audouard@fms-ea.com>
2026-08-20 10:24:47 +0200

    Classes Shape et Main

--- src/fr/entities/Shape.java
+++ src/fr/entities/Shape.java
@@ -1,3 +1,25 @@
+ 
 public class Shape {
-    
+    private Point center;
+ 
+    public Shape(int x, int y) {
+        this.center = new Point(x, y);
+    }
+ 
+    public Shape(Point center) {
+        this.center = new Point(center.getX(), center.getY());
+    }
+ 
+    public Point getCenter() {
+        return center;
+    }
+ 
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Shape [center=" + center + "]";
+    }
 }
