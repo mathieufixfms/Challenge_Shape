4bb674cdbb069d5d887aa8ecf411acfd08b58a52
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-20 11:18:10 +0200

    Modification classe Square

--- src/fr/entities/Square.java
+++ src/fr/entities/Square.java
@@ -1,36 +1,20 @@
-public class Square {
-    private int x;
-    private int y;
+public class Square extends Shape {
+
+    //Attributs
     private int side;
  
     public Square(int side, int x, int y) {
+        super(x, y);
         setSide(side);
-        setX(x);
-        setY(y);
     }
  
-    public Square(int side) {
+    public Square(int side, Point center) {
+        super(center);
         setSide(side);
-        setX(0);
-        setY(0);
+
     }
- 
-    public int getX() {
-        return x;
-    }
- 
-    public void setX(int x) {
-        this.x = x;
-    }
- 
-    public int getY() {
-        return y;
-    }
- 
-    public void setY(int y) {
-        this.y = y;
-    }
- 
+
+    //Accesseurs
     public int getSide() {
         return side;
     }
