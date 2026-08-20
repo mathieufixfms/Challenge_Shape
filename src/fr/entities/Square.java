30777a3247e83808dec6a573c859805a51e1c422
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-20 13:27:30 +0200

    Ajout de la javadoc

--- src/fr/entities/Square.java
+++ src/fr/entities/Square.java
@@ -1,17 +1,32 @@
 package fr.entities;
 
 import java.awt.*;
-
+/**
+ * Représente un carré défini par la longueur de son côté et son centre.
+ */
 public class Square extends Shape {
 
     //Attributs
     private int side;
- 
+    /**
+     * Construit un carré à partir de la longueur de son côté
+     * et des coordonnées de son centre.
+     *
+     * @param side longueur du côté du carré
+     * @param x coordonnée x du centre
+     * @param y coordonnée y du centre
+     */
     public Square(int side, int x, int y) {
         super(x, y);
         setSide(side);
     }
- 
+    /**
+     * Construit un carré à partir de la longueur de son côté
+     * et d'un point central.
+     *
+     * @param side longueur du côté du carré
+     * @param center centre du carré
+     */
     public Square(int side, Point center) {
         super(center);
         setSide(side);
@@ -27,7 +42,11 @@ public class Square extends Shape {
         if (side < 0) side = 0;
         else this.side = side;
     }
-
+    /**
+     * Dessine le carré dans le contexte graphique fourni.
+     *
+     * @param g contexte graphique utilisé pour le dessin
+     */
     @Override
     public void draw(Graphics g) {
         g.drawRect(getCenter().getX(), getCenter().getY(), getSide(), getSide());
