77891014c392c9f7e225cd59d9dd7c95190c3da8
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-20 13:23:52 +0200

    Ajout de la javadoc

--- src/fr/entities/Circle.java
+++ src/fr/entities/Circle.java
@@ -1,21 +1,36 @@
 package fr.entities;
 
 import java.awt.*;
-
+/**
+ * Représente un cercle défini par un rayon et un centre.
+ */
 public class Circle extends Shape{
 
     private double radius;
-
+    /**
+     * Construit un cercle à partir de son rayon et des coordonnées de son centre.
+     *
+     * @param radius rayon du cercle
+     * @param x coordonnée x du centre
+     * @param y coordonnée y du centre
+     */
     public Circle(double radius, int x, int y){
         super(x, y);
         setRadius(radius);
     }
 
+    /**
+     * Construit un cercle à partir de son rayon et d'un point central.
+     *
+     * @param radius rayon du cercle
+     * @param center centre du cercle
+     */
     public Circle(double radius, Point center){
         super(center);
         setRadius(radius);
     }
 
+    //Accesseurs
     public double getRadius() {
         return radius;
     }
@@ -29,6 +44,11 @@ public class Circle extends Shape{
         }
     }
 
+    /**
+     * Dessine le cercle dans le contexte graphique fourni.
+     *
+     * @param g contexte graphique utilisé pour le dessin
+     */
     @Override
     public void draw(Graphics g) {
         g.drawOval(getCenter().getX(), getCenter().getY(), (int) getRadius(), (int) getRadius());
